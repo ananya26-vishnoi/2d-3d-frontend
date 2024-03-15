21660758ed47423f056738f950a9a8df95218727
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2024-03-15 23:39:12 +0530

    url add

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ function send_data_to_backend(event) {
         mode: 'no-cors',
         body: formData,
     })
-    // .then(response => response.json()) 
-    // .then(data => {
+    .then(response => response.json()) 
+    .then(data => {
+        location.href = 'https://2d-3d.jayantkhanna.in/plotly'
         // ope google.com
         // console.log('Success:', data);
     //     window.open('http://127.0.0.1:8001/plotly', '_blank');
-    // })
+    })
     // .catch(error => console.error('Error:', error));
-    location.href = 'https://2d-3d.jayantkhanna.in/plotly'
+   
     return false
 }
